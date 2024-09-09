bbba6e19ec9c111ac87d9e0ae2a9a1aa2504ffd9
whyshouldibother <prajwalshrestha2018@gmail.com>
2024-09-10 01:53:23 +0545

    Added Difficulties + Navigation within the game

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "raylib.h"
 #include <cstdlib>
-#include <ctime>
+#include <random>
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
+
+int randGen(int max, int blockSize)
+{
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<int> dist(5, max / blockSize - 5);
+    return dist(mt) * blockSize;
+}
+
 class entity
 {
 public:
@@ -21,13 +30,25 @@ public:
     bool check()
     {
         if (x > 800)
+        {
             x = 0;
+            return false;
+        }
         if (x < 0)
+        {
             x = 800;
+            return false;
+        }
         if (y > 600)
+        {
             y = 0;
+            return false;
+        }
         if (y < 0)
+        {
             y = 600;
+            return false;
+        }
         return true;
     }
     void update(int blocksize)
@@ -35,12 +56,12 @@ public:
         x += blocksize * directionX;
         y += blocksize * directionY;
     }
-    bool grow(entity *food)
+    bool grow(entity *food, int maxX, int maxY, int blockSize)
     {
         if (x == food->x && y == food->y)
         {
-            food->x = rand() % 80 * 10;
-            food->y = rand() % 60 * 10;
+            food->x = randGen(maxX, blockSize);
+            food->y = randGen(maxY, blockSize);
             return true;
         }
         else
@@ -53,22 +74,15 @@ public:
     }
 };
 
-int randGen(int max, int blockSize)
-{
-    srand(time(0));
-    return rand() % (max / blockSize) * blockSize;
-}
-
 int main()
 {
 
-    int screenWidth = 800, screenHeight = 600, fps = 10, blockSize = 10, fontSize = 24, currMode = 0;
+    int screenWidth = 800, screenHeight = 600, fps = 10, blockSize = 10, boxOffset = blockSize * 15, fontSize = 24, currMode = 0, score = 0, difficulty;
     InitWindow(screenWidth, screenHeight, "Snake");
     SetTargetFPS(fps);
     entity food(randGen(screenWidth, blockSize), randGen(screenHeight, blockSize));
     vector<entity> snake;
     snake.push_back(entity(randGen(screenWidth, blockSize), randGen(screenHeight, blockSize)));
-    int score = 0;
     bool gameOver = false, start = false;
     while (!WindowShouldClose())
     {
@@ -103,10 +117,11 @@ int main()
                 // GameLogic
 
                 // Check for Walls
-                snake[0].check();
+                if (snake[0].check() == false && difficulty > 0)
+                    gameOver = true;
 
                 // Check Growth
-                if (snake[0].grow(&food))
+                if (snake[0].grow(&food, screenWidth, screenHeight, blockSize))
                 {
                     // Grow snake
                     snake.push_back(entity(snake[snake.size() - 1].x, snake[snake.size() - 1].y));
@@ -127,7 +142,7 @@ int main()
                 {
                     if (snake[0] == snake[i])
                     {
-                        cout<<snake.size();
+                        cout << snake.size();
                         gameOver = true;
                         break;
                     }
@@ -139,7 +154,7 @@ int main()
                 }
 
                 ClearBackground(BLACK);
-                DrawText(TextFormat("Score\t%d", score), screenWidth - MeasureText(TextFormat("Score\t%d", score), fontSize), 0, fontSize, WHITE);
+                DrawText(TextFormat("Score:%d", score), screenWidth - MeasureText(TextFormat("Score:%d", score), fontSize), 0, fontSize, WHITE);
                 DrawRectangle(food.x, food.y, 10, 10, RED);
                 DrawRectangle(snake[0].x, snake[0].y, 10, 10, DARKGREEN);
 
@@ -149,7 +164,31 @@ int main()
             {
                 BeginDrawing();
                 ClearBackground(BLACK);
-                DrawText(TextFormat("GameOver"), screenWidth / 2 - MeasureText(TextFormat("GameOver"), fontSize) / 2, (screenHeight / 2 - fontSize), fontSize, WHITE);
+                for (int i = 1; i < snake.size(); i++)
+                {
+                    DrawRectangle(snake[i].x, snake[i].y, blockSize, blockSize, GRAY);
+                }
+                DrawRectangle(snake[0].x, snake[0].y, 10, 10, DARKGRAY);
+                DrawText(TextFormat("GAMEOVER"), (screenWidth - MeasureText(TextFormat("GAMEOVER"), fontSize * 3)) / 2, blockSize, fontSize * 3, RED);
+                DrawText(TextFormat("Score:%d", score), (screenWidth - MeasureText(TextFormat("Score:%d", score), fontSize * 2.5)) / 2, blockSize * 2 + fontSize * 3, fontSize * 2.5, ORANGE);
+                DrawRectangleLines(boxOffset, boxOffset, screenWidth - boxOffset * 2, screenHeight - boxOffset * 2, WHITE);
+                const char *options[] = {
+                    "PLAY AGAIN",
+                    "MAIN MENU",
+                    "QUIT"};
+                int modes = sizeof(options) / sizeof(options[0]), spacing = ((screenHeight - 2 * boxOffset) - (modes)*fontSize) / (modes + 1);
+                for (int i = 0; i < modes; i++)
+                {
+                    DrawText(TextFormat("%s", options[i]), (screenWidth - (MeasureText(TextFormat("%s", options[i]), fontSize))) / 2, boxOffset + spacing * (i + 1) + fontSize * (i), fontSize, WHITE);
+                }
+
+                // Draw Menu
+                DrawLine(
+                    (screenWidth - (MeasureText(TextFormat("%s", options[currMode]), fontSize))) / 2 - blockSize,
+                    boxOffset + spacing * (currMode + 1) + fontSize * (currMode) + fontSize,
+                    (screenWidth + (MeasureText(TextFormat("%s", options[currMode]), fontSize))) / 2 + blockSize,
+                    boxOffset + spacing * (currMode + 1) + fontSize * (currMode) + fontSize,
+                    WHITE);
                 EndDrawing();
             }
         }
@@ -157,12 +196,12 @@ int main()
         {
             BeginDrawing();
             ClearBackground(BLACK);
-            int boxOffset = blockSize * 15;
 
             DrawText("Use SPACE for next", (screenWidth - (MeasureText(TextFormat("Use SPACE for next"), fontSize * 1.5))) / 2, blockSize, fontSize * 1.5, WHITE);
             DrawText("Use ENTER to select", (screenWidth - (MeasureText(TextFormat("Use ENTER to select"), fontSize * 1.5))) / 2, blockSize + fontSize * 1.5, fontSize * 1.5, WHITE);
             DrawRectangleLines(boxOffset, boxOffset, screenWidth - boxOffset * 2, screenHeight - boxOffset * 2, WHITE);
 
+            // Draw Menu
             const char *options[] = {
                 "Tutorial",
                 "EASY",
@@ -177,7 +216,7 @@ int main()
                 DrawText(TextFormat("%s", options[i]), (screenWidth - (MeasureText(TextFormat("%s", options[i]), fontSize))) / 2, boxOffset + spacing * (i + 1) + fontSize * (i), fontSize, WHITE);
             }
 
-            // Draw Menu
+            //Select menu
             DrawLine(
                 (screenWidth - (MeasureText(TextFormat("%s", options[currMode]), fontSize))) / 2 - blockSize,
                 boxOffset + spacing * (currMode + 1) + fontSize * (currMode) + fontSize,
@@ -203,12 +242,15 @@ int main()
                     break;
                 case 1:
                     start = true;
+                    difficulty = 0;
                     break;
                 case 2:
                     start = true;
+                    difficulty = 1;
                     break;
                 case 3:
                     start = true;
+                    difficulty = 2;
                     break;
                 case 4:
                     start = true;
@@ -216,6 +258,7 @@ int main()
                 case 5:
                     break;
                 case 6:
+                    CloseWindow();
                     break;
                 default:
                     break;
